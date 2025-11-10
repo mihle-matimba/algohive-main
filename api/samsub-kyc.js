ae2d7d70445807e0b38b9c807bad2485b7fb7b55
mihle-matimba <141812759+mihle-matimba@users.noreply.github.com>
2025-11-10 11:01:57 +0200

    Implement SamSub KYC API routes

--- api/samsub-kyc.js
+++ api/samsub-kyc.js
@@ -1,14 +1,9 @@
-// Serverless function for SamSub KYC API
 const express = require('express');
 const kycRouter = require('./samsub/kyc');
 
-// Create Express app
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Mount the router
 app.use('/api/samsub/kyc', kycRouter);
 
-// Export as serverless function
 module.exports = app;
+module.exports.config = { runtime: 'nodejs20.x' };
